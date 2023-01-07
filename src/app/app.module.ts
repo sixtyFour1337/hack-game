34535381f072b7a7f1b939e3759312ca3ae6af90
DESKTOP-5RIAD75\timon <highkidsent@gmail.com>
2023-01-07 20:15:20 +0100

    added example service to node.js - mongoDB server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TutorialService } from './providers/tutorial.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TutorialService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/providers/tutorial.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialService {
+
+  private baseUrl = 'http://locahost:8080/api/tutorials';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  get(id): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  create(data): Observable<any> {
+    return this.http.post(this.baseUrl, data);
+  }
+
+  update(id, data): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, data);
+  }
+
+  delete(id): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(this.baseUrl);
+  }
+}
